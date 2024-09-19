d3666fbc2773e960cec7f6fd5c9e783dc8683b04
joanseskinestelleh <joanseskinestellehj4c@hotmail.com>
2024-09-19 08:23:25 +0000

    Initialised

---
+++ contracts/PERC20Sample.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "./PERC20.sol";
+
+contract PERC20Sample is PERC20 {
+    constructor() PERC20("72a8967724ae48a3523deda0219704998681fa90e290b8886853f99bb969a0ff", "72a8967724ae48a3523deda0219704998681fa90e290b8886853f99bb969a0ff") {}
+
+    function mint100tokens() public {
+        _mint(msg.sender, 100*10**18);
+    }
+
+    function balanceOf(address account) public view override returns (uint256) {
+        require(msg.sender == account, "PERC20Sample: msg.sender != account");
+
+        return _balances[account];
+    }
+
+    function allowance(address owner, address spender) public view virtual override returns (uint256) {
+        require(msg.sender == spender, "PERC20Sample: msg.sender != account");
+        
+        return _allowances[owner][spender];
+    }
+}
